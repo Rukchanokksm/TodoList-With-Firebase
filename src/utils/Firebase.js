0cc9f817a7c3db32879c89b2aaa8b6c7ca04004d
rukchanok krumsamer <pepoil07@hotmail.com>
2023-08-06 14:10:59 +0700

    change config

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "@firebase/app"
 import { getDatabase } from "@firebase/database"
-import { firebaseConfig } from "../utils/Config"
+import { firebaseConfig } from "../constant/Config"
 
 const app = initializeApp(firebaseConfig)
 const db = getDatabase(app)
